0ea6e67760d220952e117e74326aeaaadee618fc
Storm101 <tristanmoore.3d@gmail.com>
2023-06-13 01:08:44 +1000

    Create basic window & game start script

--- Space Invaders/Space Invaders.cpp
+++ Space Invaders/Space Invaders.cpp
@@ -1,20 +1,25 @@
-// Space Invaders.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "raylib.h"
+#include "Game.cpp"
 
-#include <iostream>
+int main(int argc, char* argv[]) {
+	//Create game
+	Game* game = new Game();
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	//Create application window
+	InitWindow(1920 * 0.9, 1080 * 0.9, "Space Invaders");
+
+	//Set target frame rate
+	SetTargetFPS(60);
+
+	//Initialize game
+	game->Init();
+
+	//Run game
+	while (!WindowShouldClose()) {
+		game->Update();
+		game->Draw();
+	}
+
+	//Close window on end
+	CloseWindow();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
