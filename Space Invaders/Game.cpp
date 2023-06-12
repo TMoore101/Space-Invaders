041dd3120139a68d899ceffdb6d39a21eb6f04dc
Storm101 <tristanmoore.3d@gmail.com>
2023-06-13 01:18:22 +1000

    Setup Game Class

---
+++ Space Invaders/Game.cpp
@@ -0,0 +1,22 @@
+#include <raylib.h>
+#include <cmath>
+#include "GameObject.h"
+#include "SpriteObject.h"
+#include "helper.h"
+#include <iostream>
+
+class Game {
+	private:
+		//Game variables
+		int fps = 1;
+		float deltaTime = 0.005f;
+	public:
+		//Default constructor
+		Game() {}
+
+	//Initialization
+	void Init() {
+
+	}
+
+};
